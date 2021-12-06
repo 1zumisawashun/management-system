8649aa7a5c6c3539c1d9e0c8d7632b51cc866b3c
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-06 09:31:32 +0900

    add-context-provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+// for connecting user and authIsReady propaties from all components
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
