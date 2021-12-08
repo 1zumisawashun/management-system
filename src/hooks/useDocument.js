38d63d2caec471f0d4d808d810b422c749113e6a
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-08 21:15:39 +0900

    add-project-detail-page

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { projectFirestore } from "../hooks/useFirestore";
+import { projectFirestore } from "../firebase/config";
 export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
@@ -9,8 +9,12 @@ export const useDocument = (collection, id) => {
     const ref = projectFirestore.collection(collection).doc(id);
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        } else {
+          setError("no such socument exist");
+        }
       },
       (err) => {
         console.log(err);
